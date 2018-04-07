492fb61b7dc2e7e0b83f36a18ed56e7a34dd03df
engmagdy87 <eng.magdy87@gmail.com>
2018-04-07 03:50:14 +0200

    finish service worker

---
+++ sw.js
@@ -0,0 +1,68 @@
+const newCacheName = "restaurants-v2";
+
+//  install new cache
+self.addEventListener("install", function(event) {
+  //  files to be cached
+  let urlsToCache = [
+    "/",
+    "restaurant.html",
+    "css/styles.css",
+    "data/restaurants.json",
+    "img/normal/1.jpg",
+    "img/normal/2.jpg",
+    "img/normal/3.jpg",
+    "img/normal/4.jpg",
+    "img/normal/5.jpg",
+    "img/normal/6.jpg",
+    "img/normal/7.jpg",
+    "img/normal/8.jpg",
+    "img/normal/9.jpg",
+    "img/normal/10.jpg",
+    "img/small/1.jpg",
+    "img/small/2.jpg",
+    "img/small/3.jpg",
+    "img/small/4.jpg",
+    "img/small/5.jpg",
+    "img/small/6.jpg",
+    "img/small/7.jpg",
+    "img/small/8.jpg",
+    "img/small/9.jpg",
+    "img/small/10.jpg",
+    "js/dbhelper.js",
+    "js/main.js",
+    "js/restaurant_info.js"
+  ];
+
+  //  create cache and add all files to it
+  event.waitUntil(
+    caches.open(newCacheName).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+//  fetch data
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) return response;
+      return fetch(event.request);
+    })
+  );
+});
+
+//  delete old cache as new data fetched
+self.addEventListener("activate", function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames
+          .filter(
+            cacheName =>
+              cacheName.startsWith("restaurants") && cacheName !== newCacheName
+          )
+          .map(cacheName => cache.delete(cacheName))
+      );
+    })
+  );
+});
---
+++ swRegisterer.js
@@ -0,0 +1,6 @@
+if (navigator.serviceWorker) {
+  // navigator.serviceWorker.register("sw.js", {
+  //   scope: "/"
+  // });
+  navigator.serviceWorker.register("sw.js");
+}
